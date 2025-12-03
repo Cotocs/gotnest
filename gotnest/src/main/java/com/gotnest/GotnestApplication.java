dbda98aba95596cde63accbb3d3c4981b36beabb
Cotocs <matheus88.antunes@gmail.com>
2025-12-03 15:19:40 -0300

    initialize project structure with Gradle and Spring Boot setup

---
+++ gotnest/src/main/java/com/gotnest/GotnestApplication.java
@@ -0,0 +1,13 @@
+package com.gotnest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GotnestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GotnestApplication.class, args);
+	}
+
+}
---
+++ gotnest/src/test/java/com/gotnest/GotnestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gotnest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GotnestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
